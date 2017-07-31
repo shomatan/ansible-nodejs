093328735e1ff75953f12e1ff4e039dd31349348
Shoma Nishitateno <shoma416@gmail.com>
2017-07-31 20:02:14 +0900

    Add secured action.

--- app/controllers/api/v1/PostController.scala
+++ app/controllers/api/v1/PostController.scala
@@ -2,14 +2,22 @@ package me.shoma.play_cms.controllers.api.v1
 
 import javax.inject._
 
+import com.mohiva.play.silhouette.api.Silhouette
+import me.shoma.play_cms.utils.authentication.DefaultEnv
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class PostController @Inject()(cc: ControllerComponents)(implicit exec: ExecutionContext) extends AbstractController(cc) {
+class PostController @Inject()(
+                                cc: ControllerComponents,
+                                val silhouette: Silhouette[DefaultEnv])(implicit exec: ExecutionContext) extends AbstractController(cc) {
 
-  def posts = Action.async {
+  def list = Action.async {
+    Future(Ok(""))
+  }
+
+  def post = silhouette.SecuredAction.async {
     Future(Ok(""))
   }
 
