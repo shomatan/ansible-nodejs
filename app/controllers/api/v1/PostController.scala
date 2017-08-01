5aff227fb3481940d593744a39e63692eebc7880
Shoma Nishitateno <shoma416@gmail.com>
2017-08-01 14:43:28 +0900

    Listing posts.

--- app/controllers/api/v1/PostController.scala
+++ app/controllers/api/v1/PostController.scala
@@ -3,18 +3,30 @@ package me.shoma.play_cms.controllers.api.v1
 import javax.inject._
 
 import com.mohiva.play.silhouette.api.Silhouette
+import me.shoma.play_cms.models.{Category, Post, Tag}
+import me.shoma.play_cms.repositories.PostRepository
 import me.shoma.play_cms.utils.authentication.DefaultEnv
 import play.api.mvc._
+import play.api.libs.json.Json
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class PostController @Inject()(
                                 cc: ControllerComponents,
-                                val silhouette: Silhouette[DefaultEnv])(implicit exec: ExecutionContext) extends AbstractController(cc) {
+                                val silhouette: Silhouette[DefaultEnv],
+                                postRepository: PostRepository)(implicit exec: ExecutionContext) extends AbstractController(cc) {
+
+  implicit val categoryWrites = Json.writes[Category]
+  implicit val tagWrites = Json.writes[Tag]
+  implicit val postWrites = Json.writes[Post]
 
   def list = Action.async {
-    Future(Ok(""))
+
+    postRepository.list().map { case (posts) =>
+      val json = Json.toJson(posts)
+      Ok(json)
+    }
   }
 
   def post = silhouette.SecuredAction.async {
--- app/repositories/PostRepository.scala
+++ app/repositories/PostRepository.scala
@@ -3,7 +3,7 @@ package me.shoma.play_cms.repositories
 import java.time.{Instant, ZoneId, ZonedDateTime}
 import javax.inject.Inject
 
-import me.shoma.play_cms.models.Post
+import me.shoma.play_cms.models.{Category, Post}
 import play.api.db.slick.DatabaseConfigProvider
 
 import scala.concurrent.Future
@@ -12,24 +12,47 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class PostRepository @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) extends RepositorySlick {
 
   import profile.api._
-  
+
+  def list(page: Int = 0, pageSize: Int = 10): Future[Option[Post]] = {
+    val offset = pageSize * page
+    val postQuery = (for {
+      (postAndRelation, category) <- slickPosts joinLeft slickPostCategories on (_.id === _.postId) joinLeft slickCategories on (_._2.map(_.categoryId) === _.id)
+      (_, tag) <- slickPosts joinLeft slickPostTags on (_.id === _.postId) joinLeft slickTags on (_._2.map(_.tagId) === _.id)
+    } yield (postAndRelation._1, category, tag))
+      .sortBy(rows => rows._1.updatedAt.desc)
+      .drop(offset)
+      .take(pageSize)
+    db.run(postQuery.result.headOption).map { resultOption =>
+      resultOption.map {
+        case (post, category, tag) =>
+          Post(
+            post.id,
+            post.title,
+            post.content,
+            category.map{ c => Category(c.id, c.name) } toList,
+            tag.map{ t => me.shoma.play_cms.models.Tag(t.id, t.name) } toList,
+            ZonedDateTime.ofInstant(Instant.ofEpochSecond(post.createdAt), ZoneId.systemDefault()),
+            ZonedDateTime.ofInstant(Instant.ofEpochSecond(post.updatedAt), ZoneId.systemDefault())
+          )
+      }
+    }
+  }
+
   case class DBPost(
                      id: Long,
                      title: String,
                      content: String,
-                     createdAt: ZonedDateTime,
-                     updatedAt: ZonedDateTime
+                     createdAt: Long,
+                     updatedAt: Long
                    )
 
   class Posts(tag: Tag) extends Table[DBPost](tag, "posts") {
 
-    implicit val dateColumnType = MappedColumnType.base[ZonedDateTime, Long](d => d.toInstant.getEpochSecond, d => ZonedDateTime.ofInstant(Instant.ofEpochSecond(d), ZoneId.systemDefault()))
-
     def id = column[Long]("post_id", O.PrimaryKey, O.AutoInc)
     def title = column[String]("title")
     def content = column[String]("content")
-    def createdAt = column[ZonedDateTime]("created_at")
-    def updatedAt = column[ZonedDateTime]("updated_at")
+    def createdAt = column[Long]("created_at")
+    def updatedAt = column[Long]("updated_at")
 
     def * = (id, title, content, createdAt, updatedAt) <> (DBPost.tupled, DBPost.unapply _)
   }
@@ -49,7 +72,7 @@ class PostRepository @Inject() (protected val dbConfigProvider: DatabaseConfigPr
   class Tags(tag: Tag) extends Table[DBTag](tag, "tags") {
 
     def id = column[Long]("tag_id", O.PrimaryKey, O.AutoInc)
-    def name = column[String]("name")
+    def name = column[String]("tag")
 
     def * = (id, name) <> (DBTag.tupled, DBTag.unapply _)
   }
