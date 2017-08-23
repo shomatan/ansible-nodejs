a47e0330ab129f44fd63f3975834f440fbe86b0f
Shoma Nishitateno <shoma416@gmail.com>
2017-08-23 11:07:58 +0900

    Create custom field table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -60,13 +60,20 @@ CREATE TABLE post_tag (
   , CONSTRAINT pk_post_tag PRIMARY KEY (tag_id, post_id)  -- explicit pk
 );
 
-INSERT INTO users VALUES ('c622137e-d45f-4542-bd46-934d3e8a0dd7', 'Admin', 'user', 'admin@example.com', 1502153234, 1502153234);
-INSERT INTO login_info (provider_id, provider_key) VALUES ('credentials', 'admin@example.com');
+CREATE TABLE post_custom_fields (
+    post_id       int     REFERENCES posts (post_id)        ON UPDATE CASCADE ON DELETE CASCADE
+  , key_name      VARCHAR NOT NULL
+  , value         VARCHAR NOT NULL
+  , PRIMARY KEY (post_id, key_name)
+);
+
+INSERT INTO users VALUES ('c622137e-d45f-4542-bd46-934d3e8a0dd7', 'Admin', 'user', 'admin@admin.com', 1502153234, 1502153234);
+INSERT INTO login_info (provider_id, provider_key) VALUES ('credentials', 'admin@admin.com');
 INSERT INTO user_login_info VALUES ('c622137e-d45f-4542-bd46-934d3e8a0dd7', 1);
 INSERT INTO password_info VALUES ('bcrypt', '$2a$10$TgaGi7bBm9BGjVvq9H/pOucAUq6gVin.nrtDw5wz7Ux0NZyXzxieq', NULL, 1);
 
 # --- !Downs
-
+drop table post_custom_fields;
 drop table post_tag;
 drop table post_category;
 drop table tags;
